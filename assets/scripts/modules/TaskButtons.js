72f693e5e5030836ec8b7f94689c3ebeda70ec16
Jordan Deveau <jordandeveau@Jordans-Macbook.local>
2020-08-27 21:31:26 -0400

    Added modals for edit & delete buttons

--- assets/scripts/modules/TaskButtons.js
+++ assets/scripts/modules/TaskButtons.js
@@ -1,6 +1,9 @@
 export default class TaskButtons {
   constructor() {
     this.taskList = document.querySelector(".task-list")
+    this.deleteModal = document.querySelector(".delete-modal")
+    this.editModal = document.querySelector(".edit-modal")
+    this.deleteAllModal = document.querySelector(".delete-all-modal")
     if (this.taskList) {this.events()}
   }
 
@@ -11,6 +14,19 @@ export default class TaskButtons {
       this.checkBtnAction(event)
       this.editBtnAction(event)
       this.deleteAllBtnAction(event)
+      this.modalExitBtnAction(event)
+      this.modalCancelBtnAction(event)
+      this.modalUpdateBtnAction(event)
+      this.modalDeleteBtnAction(event)
+      this.modalDeleteAllBtnAction(event)
+    })
+    
+    document.addEventListener("keyup", function(event) {
+      if (event.keyCode == 27) {
+        if (document.querySelector(".modal--visible")) {
+          document.querySelector(".modal--visible").classList.remove("modal--visible")
+        }
+      }
     })
   }
 
@@ -18,45 +34,18 @@ export default class TaskButtons {
   deleteAllBtnAction(event) {
     if (event.target.classList.contains("delete-all-btn")) {
       event.preventDefault()
-      if (confirm("Are you sure you want to delete all your tasks?")) {
-        fetch("/delete-tasks", {
-          method: "POST",
-          headers: {
-            'Content-Type': 'application/json',
-          }
-        })
-        .then(() => {
-          location.reload()
-        })
-        .catch(() => {
-          alert("Error. Please try again later.")
-        })
-      }
- 
+      this.deleteAllModal.classList.add("modal--visible")
     }
   }
 
   deleteBtnAction(event) {
     if (event.target.classList.contains("delete-btn")) {
       event.preventDefault()
-      let result = confirm("Are you sure you want to delete this item?")
-      if (result) {
-        let id = event.target.getAttribute("data-id")
-        // Sends post request to delete task from database
-        fetch(`/delete/${id}`)
-        .then(() => {
-          // Removes element from frontend
-          let taskItem = event.target.parentElement.parentElement
-          taskItem.classList.add("animation")
-          taskItem.addEventListener("animationend", () => {
-            taskItem.remove()
-            this.checkIfTasks()
-          })
-        })
-        .catch(function() {
-          this.errorAlert("Please try again later.")
-        })
-      }
+      let id = event.target.getAttribute("data-id")
+      let task = event.target.parentElement.parentElement.querySelector(".task-text").innerHTML
+      this.deleteModal.classList.add("modal--visible")
+      this.deleteModal.querySelector(".modal__button--delete").setAttribute("data-id", id)
+      this.deleteModal.querySelector(".modal__text").value = task
     }
   }
 
@@ -90,28 +79,10 @@ export default class TaskButtons {
     if (event.target.classList.contains("edit-btn")) {
       event.preventDefault()
       let input = event.target.parentElement.parentElement.querySelector(".task-text").innerHTML
-      let result = prompt("Edit Your Task", input)
-      if (result) {
-        let id = event.target.getAttribute("data-id")
-        const data = { task: result };
-        // Sends post request to update task in database
-        fetch(`/edit/${id}`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(data),
-        })
-        .then(response => response.text())
-        .then(function(text) {
-          // Updates task text in frontend
-          console.log(text)
-          event.target.parentElement.parentElement.querySelector(".task-text").innerHTML = text
-        })
-        .catch(function() {
-          alert("Please try again later.")
-        })
-      }
+      let id = event.target.getAttribute("data-id")
+      this.editModal.classList.add("modal--visible")
+      this.editModal.querySelector(".modal__text").value = input
+      this.editModal.querySelector(".modal__update-btn").setAttribute("data-id", id)
     }
   }
 
@@ -144,6 +115,94 @@ export default class TaskButtons {
       <h3 class='alert-heading'>Error:</h3>
       <p class='alert-message'>${message}</p>
       </div>`
+  }
+
+  // Modal Models:
+  modalExitBtnAction(event) {
+    if (event.target.parentElement.classList.contains("modal__exit-btn")) {
+      event.target.parentElement.parentElement.classList.remove("modal--visible")
+    }
+  }
+
+  modalCancelBtnAction(event) {
+    if (event.target.classList.contains("modal__button--cancel") || event.target.classList.contains("modal__cancel-btn")) {
+      event.target.parentElement.parentElement.classList.remove("modal--visible")
+    }
+  }
+
+  modalUpdateBtnAction(event) {
+    if (event.target.classList.contains("modal__update-btn")) {
+      let result = event.target.parentElement.querySelector(".modal__text").value
+      if (result && result.length <= 100) {
+        let id = event.target.getAttribute("data-id")
+        const data = { task: result };
+        // Sends post request to update task in database
+        fetch(`/edit/${id}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        })
+        .then(response => response.text())
+        .then((text) => {
+          // Updates task text in frontend
+          this.editModal.classList.remove("modal--visible")
+          this.editModal.querySelector(".modal__text").value = ""
+          let task = document.querySelector(`[data-id='${id}']`).parentElement.parentElement.parentElement
+          task.querySelector(".task-text").innerHTML = result
+        })
+        .catch(function() {
+          this.editModal.classList.remove("modal--visible")
+          alert("Please try again later.")
+        })
+      } else {
+        this.editModal.classList.remove("modal--visible")
+      }
+    }
+  }
+
+  modalDeleteBtnAction(event) {
+    if (event.target.classList.contains("modal__button--delete")) {
+      let result = this.deleteModal.querySelector(".modal__text").value
+      let id = event.target.getAttribute("data-id")
+      if (result) {
+        // Sends post request to delete task from database
+        fetch(`/delete/${id}`)
+        .then(() => {
+          // Removes element from frontend
+          this.deleteModal.classList.remove("modal--visible")
+          let taskItem = document.querySelector(`[data-id="${id}"]`).parentElement.parentElement.parentElement
+          taskItem.classList.add("animation")
+          taskItem.addEventListener("animationend", () => {
+            taskItem.remove()
+            this.checkIfTasks()
+          })
+        })
+        .catch(function() {
+          this.errorAlert("Please try again later.")
+        })
+      }
+    }
+  }
+
+  modalDeleteAllBtnAction(event) {
+    if (event.target.classList.contains("modal__button--delete-all")) {
+      fetch("/delete-tasks", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+      .then(() => {
+        this.deleteAllModal.classList.remove("modal--visible")
+        this.taskList.innerHTML = this.emptyTaskHTML()
+        document.querySelector(".empty-task").classList.add("empty-task--visible");
+      })
+      .catch(() => {
+        alert("Error. Please try again later.")
+      })
+    }
   }
 
 }
