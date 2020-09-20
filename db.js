c7ce47676a0bf497168370f9f5b37b0b071dcd6a
Jordan Deveau <jordandeveau@Jordans-Macbook.local>
2020-09-20 12:45:19 -0400

    Added Procfile for Heroku Launch

--- db.js
+++ db.js
@@ -2,8 +2,13 @@ const mongodb = require("mongodb")
 const dotenv = require("dotenv")
 dotenv.config()
 
+let port = process.env.PORT
+if (port == null || port == "") {
+  port = 3000
+}
+
 mongodb.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
   module.exports = client
   const app = require("./app")
-  app.listen(process.env.PORT)
+  app.listen(port)
 })
